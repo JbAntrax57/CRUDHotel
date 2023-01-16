bcf09b193609607a20175d3fb93f660d947b15c6
JbAntrax57 <119278836+JbAntrax57@users.noreply.github.com>
2023-01-16 12:53:12 -0600

    subida

---
+++ template/header.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Admin| Hotel Jardin de Jaluco</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- cdn icnonos-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    
+  </head>
+  <style>
+	.logo {
+    margin: auto;
+    font-size: 10px;
+    
+    padding: 7px 10px;
+    border-radius: 50% 50%;
+    color: #000000b3;
+    
+}
+img {
+  border: 1px solid #ddd;
+  border-radius: 100%;
+  padding: 5px;
+  width: 66px;
+  }
+
+</style>
+  <body>
+      <div class="container-fluid bg-info">
+          <div class="row">
+              <div class="col-md">
+                
+                  <header class="py-3">
+                    <div class="logo">
+                    <img src="img/logohotel.png">
+                </div> 
+            </div>
+                  <h3 class="text-left">Sistema de Reservaciones Hotel Jardin de Jaluco</h3>
+                  </header>
+              </div>
+          </div>
+      </div>
+    
