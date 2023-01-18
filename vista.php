86c5ccd138f679c8e9229cc72f948086bd3109e4
Ragyo <izwael_1997@hotmail.com>
2023-01-17 22:25:47 -0600

    Reservaciones jalando

--- vista.php
+++ vista.php
@@ -2,11 +2,12 @@
 
 <?php
     include_once "model/conexion.php";
-    $sentencia = $bd -> query("select * from persona");
-    $persona = $sentencia->fetchAll(PDO::FETCH_OBJ);
+    $sentencia = $bd -> query("select * from reservaciones");
+    $reservacion = $sentencia->fetchAll(PDO::FETCH_OBJ);
     //print_r($persona);
 ?>
-
+<nav class="navbar navbar-dark bg-primary">
+</nav>
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-7">
@@ -73,10 +74,14 @@
             ?> 
 
             <!-- fin alerta -->
+            <input style="margin-left:73%; border-radius:0%" type="button" onclick = "location='reservacion.php'" value = "Agregar Reservacion" class = "btn btn-primary" ></input>
+     
             <div class="card">
+                
                 <div class="card-header">
                     Lista de Reservaciones
                 </div>
+                
                 <div class="p-10">
                     <table class="table align-middle">
                         <thead>
@@ -90,15 +95,27 @@
                         <tbody>
                             
                             <?php 
-                                foreach($persona as $dato){ 
+                                foreach($reservacion as $dato){ 
                             ?>
 
-                            <tr>
-                                <td scope="row"><?php echo $dato->codigo; ?></td>
+<tr>
+                                <td scope="row"><?php echo $dato->idReservaciones; ?></td>
+                                <td scope="row"><?php echo $dato->no_folio; ?></td>
                                 <td><?php echo $dato->nombre; ?></td>
-                                <td><?php echo $dato->edad; ?></td>
-                                <td><a class="text-success" href="editar.php?codigo=<?php echo $dato->codigo; ?>"><i class="bi bi-pencil-square"></i></a></td>
+                                <td><?php echo $dato->fecha_deps; ?></td>
+                                <td><?php echo $dato->num_telefono; ?></td>
+                                <td><?php echo $dato->lugar_residencia; ?></td>
+                                <td><?php echo $dato->fecha_llegada; ?></td>
+                                <td><?php echo $dato->fecha_salida; ?></td>
+                                <td><?php echo $dato->habitacion_id; ?></td>
+                                <td><?php echo $dato->no_personas; ?></td>
+                                <td><?php echo $dato->deposito; ?></td>
+                                <td><?php echo $dato->fecha_reservacion; ?></td>
+                                <td><?php echo $dato->tipo_pago; ?></td>
+                                <td><a class="text-success" href="editar.php?codigo=<?php echo $dato->no_folio; ?>"><i class="bi bi-pencil-square"></i></a></td>
                                 <td><a onclick="return confirm('Estas seguro de eliminar?');" class="text-danger" href="eliminar.php?codigo=<?php echo $dato->codigo; ?>"><i class="bi bi-trash"></i></a></td>
+                               <!-- <td><a class="text-success" href="vista.php?codigo=<?//php echo $dato->no_folio; ?>"><i class="bi bi-eye-fill"></i></a></td>-->
+                                <!-- <td><a onclick="return confirm('Estas seguro de eliminar?');" class="text-danger" href="eliminar.php?codigo=<?php echo $dato->codigo; ?>"><i class="bi bi-trash"></i></a></td> -->
                             </tr>
 
                             <?php 
